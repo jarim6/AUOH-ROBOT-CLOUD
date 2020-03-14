ee45398043af366f0ccd173296ebf21801047a6c
Jari Mutanen <jari@netmedia.fi>
2020-03-14 10:03:53 +0200

    general fixes

--- public/joint-values.js
+++ public/joint-values.js
@@ -1,4 +1,16 @@
+/*
 const mqtt_client = mqtt.connect('wss://jari-auoh-mqtt-broker.herokuapp.com/');
+mqtt_client.on('connect', () => {
+    console.log('connected to mqtt broker');
+    mqtt_client.subscribe('joints')
+});
+mqtt_client.on('message', (topic, message) => {
+    const joint_data = JSON.parse(message)
+    document.body.innerHTML = JSON.stringify(joint_data);
+});
+*/
+
+const mqtt_client = mqtt.connect('wss://jari-auoh-mqtt-broker.herokuapp.com//');
 mqtt_client.on('connect', () => {
     console.log('connected to mqtt broker');
     mqtt_client.subscribe('joints')
